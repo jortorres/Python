7aa06a89ac8c5d5802ea1bdc0ea0a265c4a27f33
jortorres <jordan.torres1@gmail.colm>
2023-06-30 01:04:45 -0500

    Updated

---
+++ Milage.py
@@ -0,0 +1,14 @@
+
+first_num = input("First: ")
+second_num = input("Second: ")
+
+First = int(first_num)
+Second = int(second_num)
+
+sum_total = First + Second
+
+print("Sum:", sum_total)
+
+course = "Pyton for Beginners"
+
+print(10 + 3)
